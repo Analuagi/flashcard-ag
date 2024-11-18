ed11d02b4150ae43f925576f23c8b03ed5a8cf84
Analuagi <168859154+Analuagi@users.noreply.github.com>
2024-11-18 07:47:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'O que o filho do matemático fala quando quer ir ao banheiro?',
+    'Pi-pi.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital que enxerga bem?',
+    ' Boa Vista'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é, o que é? A esfera disse para o cubo.',
+    'Deixa de ser quadrado.'
+)
+
+criaCartao(
+    'Pergunta surpresa',
+    'Eu sou seu irmão, mas você não é meu irmão. Como será que isso é possível?',
+    'Você não é meu irmÃO, porque é minha irmÃ.'
+)
